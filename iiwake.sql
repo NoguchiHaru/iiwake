d98f54e2e5b5f1b2fd49520f29141ed75dd27740
2322050 <s2322050@stu.musashino-u.ac.jp>
2024-05-24 14:50:29 +0900

    データ挿入

--- iiwake.sql
+++ iiwake.sql
@@ -6,4 +6,34 @@ CREATE TABLE iiwake_naiyou (
     day INTEGER
 );
 
-
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('神業ピアニストよみぃ無料招待コンサート', '有明シアター', '5', '24');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('EXILE TAKAHIRO LIVE TOUR 2024 ”FULL THROTTLE', '有明シアター', '5', '25');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('EXILE TAKAHIRO LIVE TOUR 2024 ”FULL THROTTLE', '有明シアター', '5', '26');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('Lil かんさい LIVE 2024 『一舞入魂』', '有明シアター', '5', '29');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('Lil かんさい LIVE 2024 『一舞入魂』', '有明シアター', '5', '30');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('Lil かんさい LIVE 2024 『一舞入魂』', '有明シアター', '5', '31');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('NAOTO PRESENTS HONEST HOUSE 2024', '有明シアター', '6', '1');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('BALLISTIK BOYZ LIVE TOUR 2024 "HIGHER EX"', '有明シアター', '6', '2');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('Makihara Noriyuki Concert 2024 "TIME TRAVELING TOUR" 2nd Season 〜Yesterday Once More〜', '有明シアター', '6', '3');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('第一生命 D.LEAGUE 23-24 CHAMPIONSHIP', '有明シアター', '6', '9');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('RAISE A SUILEN LIVE 2024「ESSENTIALS」', '有明シアター', '6', '15');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('RAISE A SUILEN LIVE 2024「ESSENTIALS」', '有明シアター', '6', '16');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('2024 JUNG YONG HWA SOLO TOUR IN JAPAN "Your City"【追加公演', '有明シアター', '6', '22');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('Makihara Noriyuki Concert 2024 "TIME TRAVELING TOUR" 2nd Season 〜Yesterday Once More〜', '有明シアター', '7', '3');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('MAZZEL 1st One Man Tour 2024 “Join us in the PARADE”', '有明シアター', '7', '4');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('Little Glee Monster Live Tour 2024 “UNLOCK!”', '有明シアター', '7', '5');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('Little Glee Monster Live Tour 2024 “UNLOCK!”', '有明シアター', '7', '6');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('Cornelius 30th Anniversary Set', '有明シアター', '7', '7');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('WANDS Live Tour 2024 ～BOLD～', '有明シアター', '7', '8');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('Yuta Furukawa 15th Anniversary Live Tour 〜Delicious Birthday〜', '有明シアター', '7', '9');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('DISH// HALL TOUR 2024', '有明シアター', '7', '10');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('DISH// HALL TOUR 2024', '有明シアター', '5', '11');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('DAIENKAI 2024', '有明シアター', '7', '13');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('DAIENKAI 2024', '有明シアター', '7', '14');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('AIMYON vs TOUR 2024"ラブ・コール2', '有明シアター', '7', '17');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('AIMYON vs TOUR 2024"ラブ・コール2', '有明シアター', '7', '18');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('Orangestar ONEMAN LIVE “And So Henceforth,” Tour', '有明シアター', '7', '19');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('「金爆はどう生きるか」～意外ともう結成20周年ツアー～', '有明シアター', '7', '20');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('「金爆はどう生きるか」～意外ともう結成20周年ツアー～', '有明シアター', '7', '21');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('第117回サマーステップコンサート～ブラボーミュージカルクラシック～', '有明シアター', '7', '25');
+INSERT INTO "iiwake_naiyou" ("ivent_name", "ivent_place", "month", "day") VALUES ('Novelbright LIVE TOUR 2024', '有明シアター', '7', '26');
